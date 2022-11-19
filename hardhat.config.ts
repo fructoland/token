ae849049292a9fdf2192d6f92bc11e5e6e1e828c
notangelmario <savin@angelmario.eu>
2022-11-19 23:48:25 +0200

    Final touches

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
+import * as dotenv from 'dotenv';
+dotenv.config();
 import "@nomicfoundation/hardhat-toolbox";
 
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY!;
+const API_URL = process.env.API_URL!;
+const POLYGONSCAN_KEY = process.env.POLYGONSCAN_KEY!;
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  	solidity: "0.8.9",
+  	networks: {
+	  polygon: {
+		  url: API_URL,
+		  accounts: [PRIVATE_KEY],
+	  },
+	},
+	etherscan: {
+		apiKey: POLYGONSCAN_KEY,
+	},
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,14 +1,15 @@
 import { ethers } from "hardhat";
 
+
 async function main() {
 	const [deployer] = await ethers.getSigners();
 
 	console.log("Deploying contracts with the account:", deployer.address);
 
-	const FructolandToken = await ethers.getContractFactory("FructolandToken");
-	const fructolandToken = await FructolandToken.deploy();
+	const FructoToken = await ethers.getContractFactory("FructoToken");
+	const fructoToken = await FructoToken.deploy();
 
-	console.log("FructoToken deployed to:", fructolandToken.address);
+	console.log("FructoToken deployed to:", fructoToken.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
