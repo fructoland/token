c403498eaa39ad4039b675bb74605217856bace1
notangelmario <savin@angelmario.eu>
2022-11-19 23:09:52 +0200

    Increased premint

--- contracts/FructoToken.sol
+++ contracts/FructoToken.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FructoToken is ERC20 {
     constructor() ERC20("Fructo Token", "FRCTO") {
-        _mint(msg.sender, 1000 * 10 ** decimals());
+        _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 }
